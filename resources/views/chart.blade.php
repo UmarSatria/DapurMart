9c8b5a2b31383ecfd90bfac02742f8966f524713
nesaathussholeha <nesaathussholeha@gmail.com>
2024-02-06 06:24:14 +0700

    coba

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -117,7 +117,7 @@
                                                 <label for="penerima" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Penerima</label>
                                                 <input type="text"  name="penerima" id="penerima" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 @error('penerima') is-invalid @enderror"  value="{{ Auth::user()->name }}" readonly >
                                             </div>
-
+mm
                                             <div class="col-span-2">
                                                 <label for="exampleInputEmail1" class="form-label">Alamat</label>
                                                 <input type="text" name="alamat" id="alamat" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white form-control @error('alamat') is-invalid @enderror"  value="{{ old('alamat') }}" placeholder="alamat" >
